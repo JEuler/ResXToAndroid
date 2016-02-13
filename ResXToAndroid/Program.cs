00223ef9d74bc69b0721e16b0fc91a6360e4defd
Ivan Terekhin <i.terhin@gmail.com>
2016-02-14 01:16:24 +0800

    to ios added

--- ResXToAndroid/Program.cs
+++ ResXToAndroid/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -10,49 +11,98 @@ using System.Xml.Linq;
 
 namespace ResXToAndroid {
 	class Program {
-		static void Main( string[] args )
+		struct XmlKeyValue
 		{
+			public string Name { get; set; }
+			public string Value { get; set; }
+		}
+
+
+		static void Main( string[] args ) {
 			// First parameter is the name of resx
-			try
-			{
-				var fileName = args[0];
-				string resxFile = fileName;
-				XDocument xDoc = XDocument.Load(resxFile);
+			try {
+#if DEBUG
+				var fileName = "strings.xml";
+				var mode = "atoi";
+#else
 
-				var result = from item in xDoc.Descendants("data")
-					select new
-					{
-						Name = item.Attribute("name").Value,
-						Value = item.Element("value").Value
-					};
+				var fileName = args[ 0 ];
+				var mode = args[1];
+#endif
 
-				XmlWriterSettings settings = new XmlWriterSettings {Indent = true};
-				XmlWriter writer = XmlWriter.Create("strings.xml", settings);
-				writer.WriteStartElement("resources");
-				foreach (var pair in result)
+				XDocument xDoc = XDocument.Load( fileName );
+				IEnumerable<XmlKeyValue> result = null;
+
+				if (mode.StartsWith("w"))
 				{
-					writer.WriteStartElement("string");
-					writer.WriteAttributeString("name", pair.Name);
-					var str = pair.Value;
-					// Apostrophe handle done with str.replace
-					writer.WriteValue( str.Replace( "'", "\\'" ) );
-					writer.WriteEndElement();
+
+					result = from item in xDoc.Descendants("string")
+						select new XmlKeyValue()
+						{
+							Name = item.Attribute("name").Value,
+							Value = item.Element("Value").Value
+						};
+				} else if (mode.StartsWith("a"))
+				{
+					result = from item in xDoc.Descendants("string")
+						select new XmlKeyValue()
+						{
+							Name = item.Attribute("name").Value,
+							Value = item.Value
+						};
 				}
-				writer.WriteEndElement();
-				writer.Close();
-				Console.WriteLine( "All done, enjoy!" );
+				// Future for ios convertion
+				else
+				{
+					
+				}
+
+				if ( mode == "wtoa" ) {
+					XmlWriterSettings settings = new XmlWriterSettings { Indent = true };
+					// ToDo change name stuff
+					XmlWriter writer = XmlWriter.Create( "strings.xml", settings );
+					writer.WriteStartElement( "resources" );
+					foreach ( var pair in result ) {
+						writer.WriteStartElement( "string" );
+						writer.WriteAttributeString( "name", pair.Name );
+						var str = pair.Value;
+						// Apostrophe handle done with str.replace
+						writer.WriteValue( str.Replace( "'", "\\'" ) );
+						writer.WriteEndElement();
+					}
+					writer.WriteEndElement();
+					writer.Close();
+					Console.WriteLine( "All done, enjoy!" );
+				}
+				else if ( mode == "atoi" ) {
+					List<string> strs = new List<string>();
+					foreach ( var pair in result ) {
+						strs.Add( string.Format( "\"{0}\": \"{1}\"", pair.Name, pair.Value ) );
+					}
+
+					File.WriteAllLines( string.Format( fileName + "_ios" ), strs );
+					Console.WriteLine( "All done, enjoy!" );
+				} else if (mode == "wtoi")
+				{
+					List<string> strs = new List<string>();
+					foreach ( var pair in result ) {
+						strs.Add( string.Format( "\"{0}\": \"{1}\"", pair.Name, pair.Value ) );
+					}
+
+					File.WriteAllLines( string.Format(fileName + "_ios"), strs );
+					Console.WriteLine( "All done, enjoy!" );
+				}
+				// handle for other cases
 			}
-			catch (FileNotFoundException)
-			{
-				Console.WriteLine("Give me normal filename, please! :-)");
+			catch ( FileNotFoundException ) {
+				Console.WriteLine( "Give me normal filename, please! :-)" );
 				Console.WriteLine( "Any key to exit:" );
 				Console.ReadLine();
 			}
 
-			catch (IndexOutOfRangeException)
-			{
-				Console.WriteLine( "You give me no file! Please, give me file!" );
-				Console.WriteLine("Any key to exit:");
+			catch ( IndexOutOfRangeException ) {
+				Console.WriteLine( "You give me no file! Please, give me file! Or give me mode: atoi, wtoi, wtoa" );
+				Console.WriteLine( "Any key to exit:" );
 				Console.ReadLine();
 			}
 		}
